df50a2cc12531122da5d0f8af41aac6987dceca3
Gajendra <gajendramalviya1512@gmail.com>
2025-06-16 15:48:42 +0530

    Updated interfaces.go, added error.go, maps.go, and tempCodeRunnerFile.go

--- interfaces.go
+++ interfaces.go
@@ -37,16 +37,48 @@ func(s student) Access() bool{
 	} 
 	return false
 }
+func (s student) String() string {
+
+	 return fmt.Sprintf("Student Name : %s , Age : %d , Grade : %c ",s.name,s.age,s.grade)
+}
+
 func main(){
 
 	var s1 info
 
 	s1=student{"Gajendra",21,'B'}
 
+	fmt.Println(s1)
+
 	fmt.Println("Show the information of student : ")
 	fmt.Println("Name : ",s1.Name())
 	fmt.Println("Age : ",s1.Age())
 	fmt.Println("Grade : ",string(s1.Grade()))
 	fmt.Println("Eligible for driving  : ",s1.Access())
+
+    fmt.Println("Interface inside main : ")
+
+	var inter interface{}
+
+	inter=10
+    fmt.Printf("\n%v : %T",inter,inter)
+
+	inter="Hello Zopdev"
+    fmt.Printf("\n%v : %T",inter,inter)
+
+	 
+    // Type assertions 
+	fmt.Println("Type assertions : ")
+
+	var i interface {}="Range"
+
+	s:=i.(string)
+	fmt.Println(" : ",s)
+
+	f,ok:=i.(float32)
+
+	fmt.Println(f,ok)
+
+
 
 }
